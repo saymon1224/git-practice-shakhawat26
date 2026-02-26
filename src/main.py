ae1a81ecb32f52f24dd8d58d89268f496afc7795
Md Shakhawat Hossen <shakhawat@Mds-MacBook-Air-2.local>
2026-02-26 02:51:31 -0500

    Add multiply function to calculator

--- src/main.py
+++ src/main.py
@@ -2,3 +2,11 @@ import datetime
 
 print("Name: Md Shakhawat Hossen")
 print("Today's Date:", datetime.date.today())
+
+
+
+from utils import add, subtract
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
